3c6d12fa14a241c96665bf5366331198170c905b
Bertrand Caron <bertrand.fx.caron@gmail.com>
2018-04-21 14:20:09 +1000

    Feature: Debug issues with  the independent

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ from os.path import basename
 from pprint import pprint
 from operator import itemgetter
 
+DEBUG = True
+
 def article_similarity_v_0(article: str, _article: str) -> float:
     return 1.0 if article == _article else 0.0
 
@@ -67,6 +69,15 @@ if __name__ == '__main__':
         article['link']: get_keyword_frequency(article['text'])
         for article in all_articles
     }
+
+    if DEBUG:
+        BAD_ARTICLE = [
+            'https://www.independent.co.uk/news/uk/politics/theresa-may-immigration-policies-ministers-responsibility-video-windrush-latest-a8314811.html',
+            'https://www.independent.co.uk/news/world/americas/us-politics/michael-cohen-lawsuit-stormy-daniels-delay-raid-fbi-donald-trump-a8315151.html',
+        ]
+
+        pprint([(article, keyword_frequency_for_link[article['link']]) for article in all_articles if article['link'] in BAD_ARTICLE])
+        exit()
 
     SIMILARITY_THRESHOLD = 0.16
 
