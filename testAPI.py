91913cdc73af34f590ae880523df40fc26a1b6eb
Bertrand Dungan <bertranddungan@gmail.com>
2018-04-22 10:48:14 +1000

    Article now allows multiple pages and has a limit

--- testAPI.py
+++ testAPI.py
@@ -4,32 +4,39 @@ from newspaper import Article, fulltext
 from requests import get
 from json import dumps
 
+article_dicts = []
+articleCount = 0
+articleHasText = True
+articleMaxRange = 3
 
 newsapi = NewsApiClient(api_key='ddac6b98882444029f877e86e6614a6a')
 
-articleList = newsapi.get_top_headlines(q="Trump", language='en', page_size=100)['articles']
+articleList = newsapi.get_top_headlines(language='en', page_size=100)['articles']
 
+while articleHasText == True:
+    for pageNumber in range(1,articleMaxRange):
+        articleList = newsapi.get_top_headlines(language='en', page_size=100, page=pageNumber)['articles']
+        for article in articleList:
+            linkText = get(article['url'], headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
 
-article_dicts = []
-i=0
-for article in articleList:
-    linkText = get(article['url'], headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
+            try:
+                articleText=fulltext(linkText.text)
+            except AttributeError:
+                articleText=('Article text is raising an error')
 
-    try:
-        articleText=fulltext(linkText.text)
-    except AttributeError:
-        articleText=('Article text is raising an error')
-    
-    article_dict = {
-        'link': article['url'],
-        'title': article['title'],
-        'text': articleText,
-    }
+            if len(articleText) < 1:
+                articleHasText = False
+            article_dict = {
+                'link': article['url'],
+                'title': article['title'],
+                'text': articleText,
+            }
 
-    article_dicts.append(article_dict)
-    print('Downloaded article: {0}'.format(article_dict['title']))
-    i+=1
-    print('Article number: {0}'.format(i))
+            article_dicts.append(article_dict)
+            print('Downloaded article: {0}'.format(article_dict['title']))
+            articleCount+=1
+            print('Article number: {0}'.format(articleCount))
+            with open('articles_V2.json', 'wt') as fh:
+                fh.write(dumps(article_dicts))
+    break
 
-with open('articles_V2.json', 'wt') as fh:
-    fh.write(dumps(article_dicts))
