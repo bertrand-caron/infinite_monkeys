76fc0c685cb27c27685b249687f5bc311fe064f7
Bertrand Caron <bertrand.fx.caron@gmail.com>
2018-05-29 12:55:41 +1000

    Feature: Add a Flask API

---
+++ flask_app/application.py
@@ -0,0 +1,129 @@
+from typing import Dict, Any
+from traceback import format_exc
+from json import loads
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+db.create_all()
+db.session.commit()
+migrate = Migrate(app, db)
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    url = db.Column(db.String(250), nullable=False)
+
+    #pairs = db.relationship()
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+class Article_Pair(db.Model):
+    __tablename__ = 'article_pairs'
+
+    article_id = db.Column(db.Integer(), db.ForeignKey('articles.id'), nullable=False)
+    _article_id = db.Column(db.Integer(), db.ForeignKey('articles.id'), nullable=False)
+    score = db.Column(db.Float(), nullable=False)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint('article_id', '_article_id'),
+        {},
+    )
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+@app.route('/articles', methods=['GET'])
+def get_all_articles():
+    return jsonify(
+        [
+            article.as_dict()
+            for article in Article.query.all()
+        ]
+    )
+
+@app.route('/articles/add', methods=['POST'])
+def add_url():
+    try:
+        payload = request.get_json(force=True)
+        new_article = Article(**payload)
+        db.session.add(new_article)
+        db.session.commit()
+        return jsonify({'status': 'success', 'new_article': new_article.as_dict()})
+    except:
+        return (
+            jsonify({
+                'msg': 'Error adding new Article',
+                'status': 'error',
+                'traceback': format_exc(),
+            }),
+            404,
+        )
+
+@app.route('/articles/<int:article_id>', methods=['GET'])
+def get_url_dict(article_id: int):
+    return jsonify(Article.query.filter(Article.id == article_id).one().as_dict())
+
+@app.route('/articles/<int:article_id>/pairs', methods=['GET'])
+def get_pairs_for_url(article_id: int):
+    return jsonify(
+        [
+            article_pair.as_dict()
+            for article_pair in
+            Article_Pair.query.filter(
+                (Article_Pair.article_id == article_id) | (Article_Pair._article_id == article_id),
+            ).all()
+        ],
+    )
+
+@app.route('/article_pairs', methods=['GET'])
+def get_all_article_pairs():
+    return jsonify(
+        [
+            article_pair.as_dict()
+            for article_pair in Article_Pair.query.all()
+        ]
+    )
+
+@app.route('/article_pairs/add', methods=['POST'])
+def add_pair():
+    payload = request.get_json(force=True)
+
+    def retrieve_or_create_url(url_str: str) -> Article:
+        maybe_url = Article.query.filter(Article.url == url_str).one_or_none()
+        if maybe_url is not None:
+            return maybe_url
+        else:
+            maybe_url = Article(url=url_str)
+            db.session.add(maybe_url)
+            db.session.flush()
+            return maybe_url
+
+    articles = [
+        retrieve_or_create_url(url_str)
+        for url_str in map(lambda s: payload[s], ('url', '_url'))
+    ]
+
+    new_article_pair = Article_Pair(
+        **dict(zip(
+            ('article_id', '_article_id'),
+            sorted([article.id for article in articles]),
+        )),
+        score=0.0,
+    )
+
+    db.session.add(new_article_pair)
+    db.session.commit()
+
+    return jsonify({'status': 'success', 'new_article_pair': new_article_pair.as_dict()})
